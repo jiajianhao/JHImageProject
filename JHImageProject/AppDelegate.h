32047b538833c5e9fd65ac8e8b51ca12da6b1296
jiajianhao <you@example.com>
2017-02-23 16:27:35 +0800

    Initial Commit

---
+++ JHImageProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JHImageProject
+//
+//  Created by admin on 2017/2/23.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
